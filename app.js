051bfc15d3cdd2b72af58f4ac00f67ce57362391
jarp <r@geeks.ms>
2020-11-12 01:49:28 -0300

    cors

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.set('key', config.key)
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 app.listen(3001, ()=>{
     console.info('server funcionando')
